009a5bbc88b27c8e3a078cf6388e35a4a1153709
Higor3D <higorgomes07@gmail.com>
2022-09-22 22:14:21 -0300

    eu

---
+++ app/src/main/java/br/com/etecia/skoutapp/teste2Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.skoutapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class teste2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.teste2_layout);
+    }
+}
